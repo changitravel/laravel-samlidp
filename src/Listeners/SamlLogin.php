128d9a7fb17342219565f717be2e980aa7a7ef77
chriswest101 <me@christophermwest.co.uk>
2024-03-08 09:17:05 +0800

    Update SamlLogin.php

--- src/Listeners/SamlLogin.php
+++ src/Listeners/SamlLogin.php
@@ -19,7 +19,11 @@ class SamlLogin
             // We don't want to do any SSO stuff yet we will 2FA the user first
         } else {
             if (in_array($event->guard, config('samlidp.guards')) && request()->filled('SAMLRequest') && ! request()->is('saml/logout')) {
-                abort(response(SamlSso::dispatchNow($event->guard)), 302);
+                try {
+                    abort(response(SamlSso::dispatchNow($event->guard)), 302);
+                } catch (\Throwable $th) {
+                    abort(response(SamlSso::dispatch($event->guard)), 302);
+                }
             }
         }
     }
